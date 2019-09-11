828e1650ab3ff2a3b76ef5c53695751f6b6d169e
vaibhav <vibhukatkar96@gmail.com>
2019-09-11 10:53:41 +0530

    .equals

--- src/main/java/EqualsAndHashCode/Car.java
+++ src/main/java/EqualsAndHashCode/Car.java
@@ -2,4 +2,17 @@ package EqualsAndHashCode;
 
 public class Car {
 
+    private String owner;
+    private String madeby;
+    private String colour;
+
+    public Car(String owner, String madeby, String colour) {
+        this.owner = owner;
+        this.madeby = madeby;
+        this.colour = colour;
+    }
+    @Override
+    public boolean equals(Object obj){
+        return false;
+    }
 }
--- src/main/java/EqualsAndHashCode/CarTest.java
+++ src/main/java/EqualsAndHashCode/CarTest.java
@@ -1,5 +1,8 @@
 package EqualsAndHashCode;
 import org.junit.Test;
+
+import java.sql.SQLOutput;
+
 import static org.junit.Assert.*;
 
 public class CarTest {
@@ -15,6 +18,10 @@ public class CarTest {
     public void stringShouldBeEqual(){
         String hello1= "Hello";
         String hello2= "Hello";
+        String hello3= "H";
+        hello3= hello3 + "ello";
+        System.out.println(hello3);
+        assertTrue(hello1.equals(hello3));
 
         assertTrue(hello1==hello2);
 
@@ -22,7 +29,10 @@ public class CarTest {
 
     @Test
     public void porscheShouldBeEqual(){
-
+        Car myPorsche1 = new Car("vaibhav","Porsche","silver");
+        Car myPorsche2 = new Car("vaibhav","Porsche","silver");
+//        myPorsche2=myPorsche1;
+//        assertTrue(myPorsche1.equals(myPorsche2));
 
     }
 }
